30cba8106fb781996494d792cc32d3a75a87577f
gabbyraw <gabrielchibueze@gmail.com>
2023-09-16 10:50:09 +0100

    corrected

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Footer (){
     return (
         <div className="footer">
-            <a href="https://twitter.com/ChibuezeEgwu"><i class="fa-brands fa-twitter"></i></a>
-            <a href="https://www.facebook.com/egwu.gabrielchibueze"><i class="fa-brands fa-facebook-f"></i></a>
-            <a href="https://www.instagram.com/angel_of_newcastle/"><i class="fa-brands fa-instagram"></i></a>
-            <a href="https://github.com/gabrielchibueze"><i class="fa-brands fa-github"></i></a>
+            <a target="_blank" href="https://twitter.com/ChibuezeEgwu"><i class="fa-brands fa-twitter"></i></a>
+            <a target="_blank" href="https://www.facebook.com/egwu.gabrielchibueze"><i class="fa-brands fa-facebook-f"></i></a>
+            <a  target="_blank" href="https://www.instagram.com/angel_of_newcastle/"><i class="fa-brands fa-instagram"></i></a>
+            <a target="_blank" href="https://github.com/gabrielchibueze"><i class="fa-brands fa-github"></i></a>
         </div>
     )
 }
